b88d7249af0d0ad7bc2542e7846d3b100b6229df
Shahbuland Matiana <44281577+shahbuland@users.noreply.github.com>
2019-05-22 01:36:47 -0400

    Update sampledata.py

--- sampledata.py
+++ sampledata.py
@@ -1,5 +1,6 @@
 import ezdatasets as ezd
 import numpy as np
 
-X = ezd.GetTrainingData("cars")
+# This program can be used to get a sample of real images from a dataset
+X = ezd.GetTrainingData("car")
 ezd.sample(X,5,5)
